476b14586c90925ae296ca316a7e0d87595b8bb3
김경민 <skykkm0810@naver.com>
2024-03-06 16:34:16 +0900

    code 복구 ㅜㅜ

--- nomad_flix/lib/main.dart
+++ nomad_flix/lib/main.dart
@@ -15,27 +15,50 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
         home: Scaffold(
-      backgroundColor: Color(0xFF181818),
+      backgroundColor: const Color(0xFF181818),
       body: Padding(
-        padding: EdgeInsets.symmetric(
+        padding: const EdgeInsets.symmetric(
           horizontal: 20,
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 80),
-            Row(),
-            SizedBox(height: 120),
-            Text(
+            const SizedBox(height: 80),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Hey, ggangmi',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                    Text(
+                      'Welcome back',
+                      style: TextStyle(
+                          color: Color.fromRGBO(255, 255, 255, 0.8),
+                          fontSize: 18),
+                    )
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(height: 120),
+            const Text(
               'Total Balance',
               style: TextStyle(fontSize: 22, color: Colors.white),
             ),
-            SizedBox(
+            const SizedBox(
               height: 10,
             ),
-            Text(
+            const Text(
               "\$5 194 482",
               style: TextStyle(
                 fontSize: 42,
@@ -43,8 +66,26 @@ class App extends StatelessWidget {
                 color: Colors.white,
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 30,
+            ),
+            Row(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(45)),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 50,
+                      ),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ))
+              ],
             )
           ],
         ),
